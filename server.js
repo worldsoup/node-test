b1945f844ac710672708ef35d69ee88fc7da7363
worldsoup <ngottlieb1@gmail.com>
2013-08-28 13:07:18 -0700

    nothing

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ console.log('Server starting');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.end('Hello World\n');
-}).listen(80, '172.31.18.76');
+}).listen(80);
 console.log('Server running at http://172.31.18.76:80/');
