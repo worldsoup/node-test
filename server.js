83572b0637168dbb851ff3d650ef43c61e2febbc
worldsoup <ngottlieb1@gmail.com>
2013-08-28 11:59:39 -0700

    remove package

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+var http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('Hello World\n');
+}).listen(80, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:80/');
