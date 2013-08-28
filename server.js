825525037a1bc5353d526c3b7df39e57e0ebcd8e
worldsoup <ngottlieb1@gmail.com>
2013-08-28 11:41:41 -0700

    s

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80);
-console.log('Server running at http://127.0.0.1:1337/');
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
